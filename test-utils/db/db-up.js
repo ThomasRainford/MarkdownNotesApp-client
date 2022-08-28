3b794491b19d1426b60871cd8f78a2c466584c9f
Thomas Rainford <thomas@rainfords.net>
2022-08-28 14:07:51 +1200

    Add basic mongosh db 'up' and 'down' scipts.

---
+++ test-utils/db/db-up.js
@@ -0,0 +1,2 @@
+// mongosh "mongodb+srv://main_user:NVCP7MeUP0MEWAgx@cluster0.qjojq.mongodb.net/testing-db?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true" ./test-utils/db/db-up.js
+db.users.insertOne({ name: "Thomas" });
