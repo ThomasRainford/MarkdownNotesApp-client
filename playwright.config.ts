83308313230033a5333d4c1201039765bbec8ec4
Thomas Rainford <thomas@rainfords.net>
2022-08-31 21:20:35 +1200

    Add GitHub workflow for running e2e tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:4000",
   },
 
   /* Configure projects for major browsers */
