a9bc3d8f6d2d718ec1acbce254559abfacb5e011
Thomas Rainford <thomas@rainfords.net>
2022-08-31 20:27:04 +1200

    Make sure to load test env when running tests.

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,13 +1,29 @@
 import { expect, test } from "@playwright/test";
+import shell from "shelljs";
 
-test("should display Next.js welcome page", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("/");
-  // The new page should contain an h1 with "Welcome to Next.js!"
-  await expect(page.locator("h2")).toContainText("MDN Notes");
-  // Click login button and wait for the page url to change.
-  await page.locator('button:has-text("Login")').click();
-  await page.waitForTimeout(1000);
+test.describe("basic tests", () => {
+  test.beforeAll(() => {
+    console.log("db ", process.env.NEXT_PUBLIC_TEST_DB_URL);
+    shell.exec(
+      `mongosh '${process.env.NEXT_PUBLIC_TEST_DB_URL}' ./test-utils/db/db-up.js`
+    );
+  });
 
-  expect(page.url()).toContain("/account/login");
+  test.afterAll(() => {
+    shell.exec(
+      `mongosh '${process.env.NEXT_PUBLIC_TEST_DB_URL}' ./test-utils/db/db-down.js`
+    );
+  });
+
+  test("should display Next.js welcome page", async ({ page }) => {
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto("/");
+    // The new page should contain an h1 with "Welcome to Next.js!"
+    await expect(page.locator("h2")).toContainText("MDN Notes");
+    // Click login button and wait for the page url to change.
+    await page.locator('button:has-text("Login")').click();
+    await page.waitForTimeout(1000);
+
+    expect(page.url()).toContain("/account/login");
+  });
 });
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,7 @@
+import { loadEnvConfig } from "@next/env";
 import "@testing-library/jest-dom/extend-expect";
+
+export default async () => {
+  const projectDir = process.cwd();
+  loadEnvConfig(projectDir);
+};
