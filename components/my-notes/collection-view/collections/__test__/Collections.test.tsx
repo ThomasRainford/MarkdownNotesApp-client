aa1568c520dba74e6c22d61cb94084275b2345b1
Thomas Rainford <thomas@rainfords.net>
2022-12-22 13:48:07 +1300

    Add tests for Collections component.

--- components/my-notes/collection-view/collections/__test__/Collections.test.tsx
+++ components/my-notes/collection-view/collections/__test__/Collections.test.tsx
@@ -1,6 +1,41 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { LocalStorageProvider } from "../../../../../contexts/LocalStorageContext";
+import { testCollections } from "../../../../../test-utils/testData";
+import { LocalStorageKeys } from "../../../../../utils/types/types";
 import Collections from "../Collections";
 
 test("Displays Collections", () => {
-  render(<Collections />);
+  localStorage.setItem(
+    LocalStorageKeys.SELECTED_COLLECTION,
+    JSON.stringify(testCollections)
+  );
+  render(
+    <LocalStorageProvider storageKey={LocalStorageKeys.SELECTED_COLLECTION}>
+      <Collections />
+    </LocalStorageProvider>
+  );
+
+  const collectionPanelHeader = screen.getByText(/collections/i);
+
+  expect(collectionPanelHeader).toBeInTheDocument();
+});
+
+test("Displays the list of collections", () => {
+  localStorage.setItem(
+    LocalStorageKeys.SELECTED_COLLECTION,
+    JSON.stringify(testCollections)
+  );
+  render(
+    <LocalStorageProvider storageKey={LocalStorageKeys.SELECTED_COLLECTION}>
+      <Collections />
+    </LocalStorageProvider>
+  );
+
+  const collections = screen.getAllByRole("heading", { name: /collection/i });
+
+  const collectionInList = collections.filter(
+    (c, i) => c.id === `collection-header-${i}`
+  );
+
+  expect(collectionInList).toHaveLength(3);
 });
---
+++ test-utils/testData.ts
@@ -0,0 +1,38 @@
+export const testCollections = [
+  {
+    _id: 1,
+    title: "Collection 1",
+    upvotes: 0,
+    lists: [
+      {
+        _id: 1,
+        title: "List 1",
+        notes: [{ _id: 1, title: "Note 1", body: "Body 1" }],
+      },
+    ],
+  },
+  {
+    _id: 2,
+    title: "Collection 2",
+    upvotes: 0,
+    lists: [
+      {
+        _id: 2,
+        title: "List 2",
+        notes: [{ _id: 2, title: "Note 2", body: "Body 2" }],
+      },
+    ],
+  },
+  {
+    _id: 3,
+    title: "Collection 3",
+    upvotes: 0,
+    lists: [
+      {
+        _id: 3,
+        title: "List 3",
+        notes: [{ _id: 3, title: "Note 3", body: "Body 3" }],
+      },
+    ],
+  },
+];
