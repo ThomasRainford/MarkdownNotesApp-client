20ad18cb04ea94c1becfb176891580289229b0e8
Thomas Rainford <thomas@rainfords.net>
2023-01-17 20:23:40 +1300

    Install markdow editor related packages and render component.

--- components/my-notes/collection-view/note-content/note-editor/NoteEditor.tsx
+++ components/my-notes/collection-view/note-content/note-editor/NoteEditor.tsx
@@ -1,11 +1,22 @@
+import "@uiw/react-markdown-preview/markdown.css";
+import "@uiw/react-md-editor/markdown-editor.css";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+
 export interface Props {
   markdownText: string;
 }
 
 const NoteEditor = ({ markdownText }: Props): JSX.Element => {
+  const [value, setValue] = useState<string | undefined>(markdownText);
+
   return (
-    <div>
-      <div>{markdownText}</div>
+    <div data-color-mode="dark">
+      <div className="wmde-markdown-var">
+        <MDEditor value={value} onChange={setValue} />
+      </div>
     </div>
   );
 };
