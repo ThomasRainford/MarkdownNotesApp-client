7498997f1d8cee72910d9207b82fc9ffb789f35f
Thomas Rainford <thomas@rainfords.net>
2023-05-29 18:03:53 +1200

    Basic layout of profile page left side
    The left side is the user details side.

--- components/profile/profile-page-container/ProfilePageContainer.tsx
+++ components/profile/profile-page-container/ProfilePageContainer.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Box, Button, Heading } from "@chakra-ui/react";
 import { UseQueryState } from "urql";
 import { Exact, UserQuery } from "../../../generated/graphql";
 
@@ -12,11 +13,11 @@ export interface Props {
 }
 
 const Error = () => {
-  return <div>Error</div>;
+  return <Box>Error</Box>;
 };
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return <Box>Loading</Box>;
 };
 
 const ProfilePageContainer = ({ user, isMe }: Props): JSX.Element => {
@@ -31,10 +32,47 @@ const ProfilePageContainer = ({ user, isMe }: Props): JSX.Element => {
   }
 
   return (
-    <div>
-      <div>{isMe ? "You" : "Another user"}</div>
-      <div>{userData?.username}</div>
-    </div>
+    <Box id="profile-page-container" display={"flex"} h={"100%"}>
+      <Box display={"flex"} h={"100%"} w={"100%"}>
+        <Box display={"flex"} justifyContent={"flex-end"} h={"100%"} w={"40%"}>
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"center"}
+            w={"60%"}
+          >
+            <Box
+              display={"flex"}
+              flexDir="column"
+              justifyContent={"start"}
+              w={"100%"}
+            >
+              <Avatar
+                size={"2xl"}
+                src={"https://avatars.dicebear.com/api/male/username.svg"}
+              />
+              <Heading>{userData?.username}</Heading>
+            </Box>
+            <Box display={"flex"} justifyContent={"center"} w={"100%"}>
+              <Button w={"100%"}>{isMe ? "Edit Profile" : "Follow"}</Button>
+            </Box>
+            <Box display={"flex"} justifyContent={"start"} w={"100%"}>
+              <Box display={"flex"}>
+                <Box mr={"2em"}>3 Following</Box>
+                <Box>3 Followers</Box>
+              </Box>
+            </Box>
+            <Box display={"flex"} justifyContent={"start"} w={"100%"}>
+              {userData?.email}
+            </Box>
+          </Box>
+        </Box>
+
+        <Box h={"100%"} w={"60%"}>
+          user data
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- pages/profile/[username]/index.tsx
+++ pages/profile/[username]/index.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
+import PrimaryLayout from "../../../components/layouts/PrimaryLayout";
 import NavBar from "../../../components/navbar/NavBar";
 import ProfilePageContainer from "../../../components/profile/profile-page-container/ProfilePageContainer";
 import { useMeQuery, useUserQuery } from "../../../generated/graphql";
@@ -23,13 +24,17 @@ const Profile: NextPageWithLayout = () => {
   const isMe = meResult.data?.me?.username === username;
 
   return (
-    <Box className="my-notes-page" h={"100%"}>
+    <Box className="profile-page" h={"100%"}>
       <NavBar user={meResult} />
       {!meResult.fetching && meResult.data ? (
         <ProfilePageContainer user={userResult} isMe={isMe} />
       ) : null}
     </Box>
   );
+};
+
+Profile.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
 };
 
 export default withUrqlClient(createUrqlClient)(Profile);
