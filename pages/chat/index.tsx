6ebb2f3cf58465e9e7da8fde5111cc587a0280d9
Thomas Rainford <thomas@rainfords.net>
2023-12-19 10:17:10 +1300

    Add 'chat' page.

---
+++ pages/chat/index.tsx
@@ -0,0 +1,30 @@
+import { Box } from "@chakra-ui/react";
+import { withUrqlClient } from "next-urql";
+import SelectedDataProvider from "../../components/helper/SelectedDataProvider";
+import PrimaryLayout from "../../components/layouts/PrimaryLayout";
+import NavBar from "../../components/navbar/NavBar";
+import { useMeQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+import { useIsAuth } from "../../utils/hooks/useIsAuth";
+import { NextPageWithLayout } from "../page";
+
+const Chat: NextPageWithLayout = () => {
+  const [meResult] = useMeQuery();
+
+  useIsAuth(meResult);
+
+  return (
+    <Box className="profile-page" h={"100%"}>
+      <SelectedDataProvider>
+        <NavBar user={meResult} />
+        {!meResult.fetching && meResult.data ? <>chat page</> : null}
+      </SelectedDataProvider>
+    </Box>
+  );
+};
+
+Chat.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
+
+export default withUrqlClient(createUrqlClient)(Chat);
