289c5294ed2288011f042982cfd6e3a16efe5238
Thomas Rainford <thomas@rainfords.net>
2023-12-21 13:59:15 +1300

    Add message input.

--- components/chat/chat-container/chat-messages/ChatMessages.tsx
+++ components/chat/chat-container/chat-messages/ChatMessages.tsx
@@ -1,4 +1,12 @@
-import { Avatar, Box, Heading } from "@chakra-ui/react";
+import { ArrowRightIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  ButtonGroup,
+  Heading,
+  IconButton,
+  Input,
+} from "@chakra-ui/react";
 import { ChatPrivate, ChatRoom } from "../../../../generated/graphql";
 
 const ChatMessageHeader = ({
@@ -33,14 +41,19 @@ const ChatMessages = ({ chat }: Props): JSX.Element => {
       flexDirection="column"
       h={"100%"}
       w={{ base: "100%", md: "50%" }}
-      padding="1em"
       bg="gray.600"
+      height={"100%"}
     >
       {!chat ? (
         <></>
       ) : (
-        <Box>
-          <Box mb={"2em"}>
+        <Box
+          display={"flex"}
+          justifyContent="space-between"
+          flexDir="column"
+          height={"100%"}
+        >
+          <Box mb={"2em"} padding="1em">
             {chat.__typename === "ChatPrivate" ? (
               <ChatMessageHeader name={chat.participants[1].username} />
             ) : chat.__typename === "ChatRoom" ? (
@@ -49,15 +62,38 @@ const ChatMessages = ({ chat }: Props): JSX.Element => {
               <></>
             )}
           </Box>
-          <Box>
-            {messages?.map((message) => {
-              return (
-                <Box key={message.id}>
-                  <Box> {message.sender.username}</Box>
-                  <Box> {message.content}</Box>
-                </Box>
-              );
-            })}
+          <Box flexDir="column" width={"100%"}>
+            <Box padding="1em">
+              {messages?.map((message) => {
+                return (
+                  <Box key={message.id}>
+                    <Box> {message.sender.username}</Box>
+                    <Box> {message.content}</Box>
+                  </Box>
+                );
+              })}
+            </Box>
+            <Box display="flex" m={"0.5em"}>
+              <Box flexGrow={4} mr="0.5em">
+                <Input
+                  type={"filled"}
+                  bg="gray.700"
+                  placeholder="Type your message here..."
+                  onClick={() => {}}
+                />
+              </Box>
+              <Box display="flex" alignItems="center" flexGrow={1}>
+                <ButtonGroup spacing="1">
+                  <IconButton
+                    variant={"outline"}
+                    icon={<ArrowRightIcon />}
+                    color="blue.500"
+                    bg={"gray.600"}
+                    aria-label={"send-message-button"}
+                  />
+                </ButtonGroup>
+              </Box>
+            </Box>
           </Box>
         </Box>
       )}
