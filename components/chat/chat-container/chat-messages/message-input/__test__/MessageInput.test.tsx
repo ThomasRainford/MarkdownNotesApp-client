c48c6d03132bb3d326968c78eb6c3ee28d0a5b33
Thomas Rainford <thomas@rainfords.net>
2024-01-14 12:21:46 +1300

    Fix unit tests.

--- components/chat/chat-container/chat-messages/message-input/__test__/MessageInput.test.tsx
+++ components/chat/chat-container/chat-messages/message-input/__test__/MessageInput.test.tsx
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react";
+import { testChatPrivates } from "../../../../../../test-utils/testData";
 import MessageInput from "../MessageInput";
 
 describe("MessageInput component", () => {
   test("Displays the component successfully", () => {
-    render(<MessageInput />);
+    render(<MessageInput chat={testChatPrivates[0]} />);
   });
 });
